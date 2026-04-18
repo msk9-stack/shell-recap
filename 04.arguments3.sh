ae052a2ed5c6fd00258ebf06105da0474a70bf26
Krish <msk972645@gmail.com>
2026-04-18 15:38:25 +0530

    recap

---
+++ 04.arguments3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Just changeing the variable values, no need to give aruguments from outside
+#we can just change the names and the conversation wiil change accordingly.
+
+NAME1="sharukh"
+NAME2="salman"
+
+echo "$NAME1:: hi $NAME2, how are you?"
+echo "$NAME2:: hi $NAME1, i am good, thanks for asking $NAME1. how are you doing"
+echo "$NAME1:: thank you too $NAME2, its been one year since met"
+echo "$NAME2:: yes $NAME1, i really missed you in this past one year"
