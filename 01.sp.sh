133e3c278000d9b91668df994a4a34457afa8d0b
Krish <msk972645@gmail.com>
2026-04-18 15:01:06 +0530

    without variables

---
+++ 01.sp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#this script is for creating without variables
+
+echo "siva:: hi krishna, how are you?"
+echo "krishna:: hi siva, i am good, thanks for asking siva. how are you doing"
+echo "siva:: thank you too krishna, its been one year since met"
+echo "krishn:: yes siva, i really missed you in this past one year"
