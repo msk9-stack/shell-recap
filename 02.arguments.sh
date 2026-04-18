246b1e2e0c622c5397daeeabda334aac2d79662f
Krish <msk972645@gmail.com>
2026-04-18 15:09:19 +0530

    recap

---
+++ 02.arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#this script is for creating with variables
+
+NAME1=$1
+NAME2=$2
+
+echo "$NAME1:: hi $NAME2, how are you?"
+echo "$NAME2:: hi $NAME1, i am good, thanks for asking $NAME1. how are you doing"
+echo "$NAME1:: thank you too $NAME2, its been one year since met"
+echo "$NAME2:: yes $NAME1, i really missed you in this past one year"
