5e2ade9bf6d64889a0c71868285d5a2d516bc17b
Krish <msk972645@gmail.com>
2026-04-18 15:23:28 +0530

    recap

--- 03.arguments2.sh
+++ 03.arguments2.sh
@@ -10,7 +10,7 @@ if [ $# -lt 2 ]; then
 	exit 1
 fi
 
-echo "siva:: hi krishna, how are you?"
-echo "krishna:: hi siva, i am good, thanks for asking siva. how are you doing"
-echo "siva:: thank you too krishna, its been one year since met"
-echo "krishn:: yes siva, i really missed you in this past one year"
+echo "$NAME1:: hi $NAME2, how are you?"
+echo "$NAME2:: hi $NAME1, i am good, thanks for asking $NAME1. how are you doing"
+echo "$NAME1:: thank you too $NAME2, its been one year since met"
+echo "$NAME2:: yes $NAME1, i really missed you in this past one year"
