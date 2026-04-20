100a15ffed7700a52112f85e81ffa26b50cd47dc
Krish <msk972645@gmail.com>
2026-04-20 16:03:54 +0530

    read command

--- 08.ENV.sh
+++ 08.ENV.sh
@@ -2,13 +2,11 @@
 
 # using ENV variables from the shell
 
-echo "export gold_rate-gms="10000""
-echo ""$gold_rate-gms""
-
+echo "export gold_rate_gms=10000"
+echo "\echo "$gold_rate_gms"
+echo
 echo "export -->is a command"
-echo ""
-
-echo "gold_rate-gms -->is environmental variable, which was created by export command"
-echo ""
-
-echo "echo "$gold_rate-gms" -->will print the value 10000"
+echo 
+echo "gold_rate_gms -->is environmental variable, which was created by export command"
+echo 
+echo "echo "\$gold_rate_gms" -->will print the value 10000"
