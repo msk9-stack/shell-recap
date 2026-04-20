6ff16d95b383ca409911e8ec9157acaf4da6eeab
Krish <msk972645@gmail.com>
2026-04-20 17:36:07 +0530

    read command

--- 09.read&ENV.sh
+++ 09.read&ENV.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 echo
-printf "learning both read and ENV in this script"
+printf "learning both read and ENV in this script\n"
 echo
-printf "let's see the read command"
+printf "let's see the read command\n"
 echo
-printf 'read -s -p "enter your paasword: " password -->it's a security and you can't see your password while typing it'
+printf 'read -s -p "enter your paasword: " password -->it's a security and you can't see your password while typing it\n'
 echo 
-printf 'echo "$password" --> this will print your password (for practice purpose, don't use it in real environment'
+printf 'echo "$password" --> this will print your password (for practice purpose, dont use it in real environment\n'
 echo
 echo
 echo "let's see ENV"
 echo
-printf 'export name=kiran'
+printf 'export name=kiran\n'
 echo
-printf 'export -->is a command
+printf 'export -->is a command\n'
 echo
-printf "name -->will become environmental variable"
+printf "name -->will become environmental variable\n"
 echo
-printf 'echo "$name" -->this will print the name kiran'
+printf 'echo "$name" -->this will print the name kiran\n'
 echo
-printf 'unset --> is a command to remove the env variable'
+printf 'unset --> is a command to remove the env variable\n'
 echo
 echo "example"
 echo
-printf 'unset name=kiran'
+printf 'unset name\n'
 echo
 echo
-printf 'example of read command'
+printf 'example of read command\n'
 echo
 echo
 read -s -p "enter your paasword: " password
@@ -35,5 +35,6 @@ echo
 echo "$password"
 echo
 echo
-printf 'for ENV try the export syntax in the shell'
+printf 'for ENV try the export syntax in the shell\n'
+echo
 
