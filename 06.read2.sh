72140ed88a46c19fca74a1fb1f4c478193453166
Krish <msk972645@gmail.com>
2026-04-20 13:50:26 +0530

    read command

--- 06.read2.sh
+++ 06.read2.sh
@@ -6,3 +6,8 @@ echo "what is the capital city of india"
 read -s capital
 
 echo "the capital city of india is: $capital"
+
+
+read -ps "what is the capital city of maharastra: " capital
+
+echo "the capital city of maharastra is: $capital
