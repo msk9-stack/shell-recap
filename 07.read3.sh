6a38a08ab55cc034a99b58e2ab2ca52557ace5c5
Krish <msk972645@gmail.com>
2026-04-20 14:48:15 +0530

    read command

--- 07.read3.sh
+++ 07.read3.sh
@@ -2,20 +2,38 @@
 
 echo "who hit the first double hundred in the cricket odi"
 
+echo""
+
 read name
+
+echo""
 
 echo "the first double hundred was hit by: $name"
 
+echo""
+
 echo "who made the highest double centuries in the cricket odi's"
+
+echo""
 
 read -s name
 
+echo""
+
 echo "the highest double centuries was made by $name"
+
+echo""
 
 read -p "what was the highest score in the odi history ever: " score
 
+echo""
+
 echo "the highest score ever made in the odi history was $score"
 
+echo""
+
 read -s -p "who is the king of cricket: " king
+
+echo""
 
 echo "king $king"
