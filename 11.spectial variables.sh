6fc34a74ef5aeb1e5ab7f80bfef0b633f47524d2
Krish <msk972645@gmail.com>
2026-04-22 19:30:21 +0530

    read command

--- 11.spectial variables.sh
+++ 11.spectial variables.sh
@@ -1,11 +1,31 @@
-#!/bin/bash
+#!bin/bash
 
-echo "All variables passed to the script: $@"
-echo "All variables passed to the script: $*"
-echo "Script name: $0"
-echo "Current directory: $PWD"
-echo "Who is running this: $USER"
-echo "Home directory of user: $HOME"
-echo "PID of this script: $$"
+printf 'let's learn special variables in this script\n'
+printf 
+
+printf  'to pass all the arguments: $@\n'
+printf 
+
+printf  'to pass all the arguments: $*\n'
+printf 
+
+printf  'to print the script name: $0\n'
+printf 
+
+printf  'to find who is running this script: $USER\n'
+printf 
+
+printf 'to find the user's home directory: $HOME\n'
+printf 
+
+printf 'to find which directory we are currently in(present working directory): $PWD\n'
+printf 
+
+printf 'to find the PID of the file: $$\n'
+printf 
+
 sleep 50 &
-echo "PID of the last command in background is: $!"
+
+printf 'to find last PID of the file: $!\n'
+printf 
+
