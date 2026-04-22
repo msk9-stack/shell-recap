28caa24720f442d7461ba19b578b6523cdddb2a4
Krish <msk972645@gmail.com>
2026-04-22 19:33:43 +0530

    read command

--- 11.spectial_variables.sh
+++ 11.spectial_variables.sh
@@ -1,31 +1,31 @@
 #!bin/bash
 
-printf 'let's learn special variables in this script\n'
-printf 
+printf "let"s learn special variables in this script\n"
+echo 
 
-printf  'to pass all the arguments: $@\n'
-printf 
+printf  "to pass all the arguments: $@\n"
+echo 
 
-printf  'to pass all the arguments: $*\n'
-printf 
+printf  "to pass all the arguments: $*\n"
+echo
 
-printf  'to print the script name: $0\n'
-printf 
+printf  "to print the script name: $0\n"
+echo 
 
-printf  'to find who is running this script: $USER\n'
-printf 
+printf  "to find who is running this script: $USER\n"
+echo 
 
-printf 'to find the user's home directory: $HOME\n'
-printf 
+printf "to find the user"s home directory: $HOME\n"
+echo 
 
-printf 'to find which directory we are currently in(present working directory): $PWD\n'
-printf 
+printf "to find which directory we are currently in(present working directory): $PWD\n"
+echo 
 
-printf 'to find the PID of the file: $$\n'
-printf 
+printf "to find the PID of the file: $$\n"
+echo 
 
 sleep 50 &
 
-printf 'to find last PID of the file: $!\n'
-printf 
+printf "to find last PID of the file: $!\n"
+echo 
 
