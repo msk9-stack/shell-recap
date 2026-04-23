b1d2b4ff5ec1ee3bc3b4679ff0762f9c50df2cb7
Krish <msk972645@gmail.com>
2026-04-23 14:58:58 +0530

    read command

--- 12.data_types.sh
+++ 12.data_types.sh
@@ -4,13 +4,11 @@ echo "this script will demonstrate what are the date types and how they work"
 echo
 printf "if we take creicketer's as an examples, let's see below\n"
 echo
-
 BATTERS=4
 BOWLERS=3
 ALL_ROUNDERS=4
 GAME=cricket
 echo
-
 TOTAL=$(($BATTERS+$BOWLERS+$ALL_ROUNDERS+GAME))
 echo
 
@@ -27,5 +25,5 @@ echo "OPENER2: ${BATTER_NAMES[1]}"
 echo
 echo "ONE_DOWN: ${BATTER_NAMES[2]}"
 echo
-echo "TWO_DOWN: ${[BATTER_NAMES[3]}"
+echo "TWO_DOWN: ${BATTER_NAMES[3]}"
 echo
