138759e030a66e9227d2aba1825854d9286bc56a
Krish <msk972645@gmail.com>
2026-04-25 17:52:50 +0530

    read command

--- 15.sys_memory.sh
+++ 15.sys_memory.sh
@@ -10,11 +10,18 @@ DISK_USAGE=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
 AVAILABLE_MEMORY=$(free -m | awk 'NR==2 {print $7}')
 echo
 
+echo "disk usage is: $DISK_USAGE"
+echo
+
 if [ $DISK_USAGE -lt 70 ]; then
 	echo "Disk Warning"
 else
 	echo "Disk OK"
 fi
+
+echo
+echo "available memory is: $AVAILABLE_MEMORY"
+echo
 
 if [ $AVAILABLE_MEMORY -lt 500 ]; then
 	echo "Memory Low"
@@ -22,11 +29,8 @@ else
 	echo "Memory OK"
 fi
 
-echo
-echo "available memory is: $AVAILABLE_MEMORY
-echo
 
-echo "disk usage is: $DISK_USAGE"
-echo
+
+
 
 
