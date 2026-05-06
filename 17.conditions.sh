101af290ec40ea79456833a448bb1f67864a052d
Krish <msk972645@gmail.com>
2026-05-06 08:55:56 +0530

    recap

--- 17.conditions.sh
+++ 17.conditions.sh
@@ -16,8 +16,6 @@ if [ "$NUMBER" -lt 10 ]; then
 	echo "the given number $NUMBER is: less than 10"
 elif [ "$NUMBER" -eq 10 ]; then
 	echo "the given number $NUMBER is: equal to 10"
-elif [ "$NUMBER" -ne 10 ]; then
-	echo "the given number $NUMBER is: not equal to 10"
 else
 	echo "the given number $NUMBER is: greater than 10"
 fi
