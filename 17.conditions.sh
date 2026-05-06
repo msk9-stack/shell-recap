b49eed071db471090f55461f47f76b5b546109a7
Krish <msk972645@gmail.com>
2026-05-06 08:51:51 +0530

    recap

--- 17.conditions.sh
+++ 17.conditions.sh
@@ -10,16 +10,16 @@ printf '-eq\n'
 printf '-ne\n'
 echo
 
-NUMBER=$10
+NUMBER=$1
 
 if [ "$NUMBER" -lt 10 ]; then
-	echo "the given number $NMBER is: less than 10"
+	echo "the given number $NUMBER is: less than 10"
 elef [ "$NUMBER" -eq 10 ]; then
-	echo "the given number $NMBER is: eqal to 10"
+	echo "the given number $NUMBER is: eqal to 10"
 elef [ "$NUMBER" -ne 10 ]: then
-	echo "the given number $NMBER is: not equal to 10"
+	echo "the given number $NUMBER is: not equal to 10"
 else
-	echo "the given number $NMBER is: greater than 10"
+	echo "the given number $NUMBER is: greater than 10"
 fi
 
 echo
